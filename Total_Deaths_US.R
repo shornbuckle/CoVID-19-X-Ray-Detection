228363b30def9e445341bbe14e3973feff84ba53
shornbuckle <65291545+shornbuckle@users.noreply.github.com>
2020-05-15 20:23:05 -0400

    Add files via upload

---
+++ Total_Deaths_US.R
@@ -0,0 +1,19 @@
+#This Script reads in the CSV with all of the US dates by Day
+#And Creates a plot that shows the deaths over time.
+#The data was obtained here: https://github.com/nytimes/covid-19-data
+
+library(ggplot2)
+library(scales)
+library(lubridate)
+
+us.data <- read.csv(file="us.csv")
+
+new.date <- as.POSIXct(us.data$date,format="%Y-%m-%d")
+
+#replace columns
+us.data$date <- new.date
+
+
+p1 <- ggplot() + geom_line(aes(y = deaths, x = date, group = 1),
+                           data = us.data)
+p1
